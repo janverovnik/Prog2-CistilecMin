baddaffaccd9e356b92a049727f647976a614d19
MatejFMF <mk54674@student.uni-lj.si>
2025-04-09 09:44:24 +0200

    Dodal struct Mreza

--- src/mine_field/strukture.rs
+++ src/mine_field/strukture.rs
@@ -12,10 +12,15 @@ enum Vsebina {
 }
 
 struct Tile {
-    vsebina : Vsebina,
-    status : Status,
+    vsebina: Vsebina,
+    status: Status,
+    mesto: (u16, u16)
 }
 
+pub struct Mreza {
+    velikost: (u16,u16),
+    tiles: HashMap<(u32,u32), Tile>,
+}
 // let field = HashMap<(u32,u32), Tile>
 
 
@@ -27,12 +32,36 @@ impl Tile {
     pub fn status(&self) -> Status {
         self.status
     }
+}
 
+impl Mreza {
+    pub fn tile(&self,i:u16,j: u16) -> Option<Tile> {
+        self.tiles.get((i, j))
+    }
+    pub fn add_tile(&self, tile:Tile ,i:u16 ,j:u16) -> () {
+        self.tiles.insert((i,j),tile)
+    }
+}
     
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::state;
+
+    #[test]
+    fn poizkus() {
+        let test_mreza = Mreza {
+            velikost: (123,456),
+            tiles: HashMap::new()
+        };
+        test_mreza
+    }
+}
+
 
     // fn preverjaj(&self) -> bool {
     //     match self.vsebina {
 
     //     }
     // }
-}
