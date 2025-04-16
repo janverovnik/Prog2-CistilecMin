804e4942d3f5e3d81adc18e716006547df88c7dd
MatejFMF <mk54674@student.uni-lj.si>
2025-04-16 16:10:34 +0200

    Dodal Mreza::safe_new()

--- src/generator_polja.rs
+++ src/generator_polja.rs
@@ -5,6 +5,14 @@ use rand::Rng;
 use crate::strukture::{Tile,Mreza};
 
 
+fn razlika(m:u16,n:u16) -> u16 {
+    if m > n {
+        m - n
+    } else {
+        n - m
+    }
+}
+
 impl Mreza {
     pub fn new(velikost : (u16,u16), st_min: u16) -> Mreza {
         let mut mreza = Mreza::prazna(velikost);
@@ -27,7 +35,36 @@ impl Mreza {
         };
         mreza
     }
+
+    pub fn safe_new(velikost: (u16,u16),st_min: u16, safe_space: (u16,u16)) -> Mreza {
+        let (m, n) = (velikost.0 - 1, velikost.1 - 1);
+        let (s0,s1) = safe_space;
+        let mut mreza = Mreza::prazna(velikost);
+        // Ocaml match je superior od rust matcha
+        let varne = if safe_space == (0,0) || safe_space == (m, 0) || safe_space == (0, n) || safe_space == (m, n) {
+            4
+        } else if s0 == 0 || s0 == m || s1 == 0 || s1 == n {
+            6
+        } else {
+            9
+        };
+        let mut zaporedje =  random_array(velikost.0 * velikost.1 - varne, st_min).into_iter();
+        let mut naslednji : bool;
+        for i in 0..velikost.0 {
+            for j in 0..velikost.1 {
+               if razlika(i,s0) > 1 || razlika(j,s1) > 1 {
+                naslednji = zaporedje.next().is_some();
+                        if naslednji {
+                            mreza.add_tile(Tile::new_bomb(), (i,j));
+                        };
+                    }
+                }
+            }
+            mreza  
+        }
+
 }
+
 
 fn random_array(mut st_vseh:u16,mut st_min:u16) -> Vec<bool> {
     let mut rng = rand::rng();
