560199d532580d2054dd8a89c92a6ba3ffb8538d
MatejFMF <mk54674@student.uni-lj.si>
2025-04-16 15:08:57 +0200

    Dokoncal Mreza.new()

--- src/generator_polja.rs
+++ src/generator_polja.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
 use rand::Rng;
 
@@ -8,25 +8,32 @@ use crate::strukture::{Tile,Mreza};
 impl Mreza {
     pub fn new(velikost : (u16,u16), st_min: u16) -> Mreza {
         let mut mreza = Mreza::prazna(velikost);
-        let mut zaporedje = random_array(velikost.0 * velikost.1, st_min).iter();
-        let mut naslednji : bool
+        let mut zaporedje = random_array(velikost.0 * velikost.1, st_min).into_iter();
+        let mut naslednji : bool;
         for i in 0..velikost.0 {
             for j in 0..velikost.1 {
-                naslednji = zaporedje.next()
+                naslednji = zaporedje.next().is_some();
                 if naslednji {
-                    mreza.add_tile(Tile::new_bomb(i,j), (i,j));
+                    mreza.add_tile(Tile::new_bomb(), (i,j));
                 }
             }
         };
-        
+        for i in 0..velikost.0 {
+            for j in 0..velikost.1 {
+                if mreza.je_prazno((i,j)) {
+                    mreza.add_tile(Tile::new_number(mreza.pripisi_stevilo((i, j))),(i,j));
+                }
+            }
+        };
+        mreza
     }
 }
 
-fn random_array(st_vseh:u16,st_min:u16) -> Vec<bool> {
+fn random_array(mut st_vseh:u16,mut st_min:u16) -> Vec<bool> {
     let mut rng = rand::rng();
-    let nakljucno = vec![];
+    let mut nakljucno = vec![];
     loop {
-        if rng.random_ratio(st_min,st_vseh) {
+        if rng.random_ratio(st_min.into(),st_vseh.into()) {
             nakljucno.push(true);
             st_min -= 1;
             st_vseh -= 1;
@@ -40,3 +47,4 @@ fn random_array(st_vseh:u16,st_min:u16) -> Vec<bool> {
     }
     nakljucno
 }
+
--- src/strukture.rs
+++ src/strukture.rs
@@ -38,26 +38,22 @@ impl Tile {
     pub fn status(&self) -> &Status {
         &self.status
     }
-<<<<<<< HEAD
 
-    pub fn new_bomb(mesto:(u16,u16)) -> Tile {
+    pub fn new_bomb() -> Tile {
         Tile {
             vsebina: Vsebina::Mina,
             status: Status::Closed(Mark::NotFlagged),
-            mesto: mesto,
+            // mesto: mesto,
         }
     }
     
-    pub fn new_number(mesto: (u16, u16),stevilo) -> Tile {
+    pub fn new_number(stevilo: u8) -> Tile {
         Tile {
             vsebina: Vsebina::Stevilo(stevilo),
             status: Status::Closed(Mark::NotFlagged),
-            mesto: mesto,
+            // mesto: mesto,
         }
     }
-=======
-    
->>>>>>> 85dbed522c10e11647408c764ded585c4da0a20a
 }
 
 use crate::strukture::Vsebina::Mina;
@@ -66,48 +62,52 @@ impl Mreza {
     pub fn tile(&self, mesto:(u16,u16)) -> Option<&Tile> {
         self.tiles.get(&mesto)
     }
-<<<<<<< HEAD
     pub fn add_tile(&mut self, tile:Tile ,mesto:(u16,u16)) -> Option<Tile>  {
         self.tiles.insert(mesto,tile)
     }
 
+    // pub fn add_tile(&mut self, tile: Tile, i: u16, j: u16) -> Option<Tile> {
+        //     self.tiles.insert((i, j), tile)
+        pub fn mines(&self) -> Vec<(u16, u16)> {
+            let mut mine_vec = vec![];
+            for ((i, j), tile) in self.tiles.iter() {
+                if tile.vsebina == Mina {
+                    mine_vec.push((*i, *j));
+                }
+            }
+            return mine_vec;
+        }
+        pub fn sosedje(&self, mesto:(u16,u16)) -> Vec<(u16, u16)> {
+            let (i,j) = mesto;
+            let mut mozni = vec![(i - 1, j - 1), (i, j - 1), (i + 1, j - 1),
+            (i - 1, j), (i + 1, j),
+        (i - 1, j + 1), (i, j + 1), (i + 1, j + 1)];
+        let keys: Vec<&(u16, u16)>  = self.tiles.keys().collect();
+        mozni.retain(|n| keys.contains(&n));
+        return mozni;
+    }
+    pub fn pripisi_stevilo(&self, mesto:(u16,u16)) -> u8 {
+        let (i,j) = mesto;
+        2 //TODO
+    }
+    
     pub fn prazna(velikost: (u16,u16)) -> Mreza {
         Mreza {
             velikost: velikost,
             tiles: HashMap::new(),
         }
-=======
-    pub fn add_tile(&mut self, tile: Tile, i: u16, j: u16) -> Option<Tile> {
-        self.tiles.insert((i, j), tile)
     }
-    pub fn mines(&self) -> Vec<(u16, u16)> {
-        let mut mine_vec = vec![];
-        for ((i, j), tile) in self.tiles.iter() {
-            if tile.vsebina == Mina {
-                mine_vec.push((*i, *j));
-            }
-        }
-        return mine_vec;
-    }
-    pub fn sosedje(&self, i: u16, j: u16) -> Vec<(u16, u16)> {
-        let mut mozni = vec![(i - 1, j - 1), (i, j - 1), (i + 1, j - 1),
-                         (i - 1, j), (i + 1, j),
-                         (i - 1, j + 1), (i, j + 1), (i + 1, j + 1)];
-        let keys: Vec<&(u16, u16)>  = self.tiles.keys().collect();
-        mozni.retain(|n| keys.contains(&n));
-        return mozni;
-    }
-    pub fn pripisi_stevilo(&self, i: u16, j: u16) -> u8 {
-        2 //TODO
->>>>>>> 85dbed522c10e11647408c764ded585c4da0a20a
+
+    pub fn je_prazno(&self,mesto:(u16,u16)) -> bool {
+        !self.tiles.contains_key(&mesto)
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn poizkus() {
+    
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn poizkus() {
         let test_mreza = Mreza {
             velikost: (123, 456),
             tiles: HashMap::new()
