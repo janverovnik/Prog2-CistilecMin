3fbd014e569bd56dcc00ccb48571a8d6d846c3b7
janverovnik <janverovnik4@gmail.com>
2025-05-07 08:33:26 +0200

    TODO funckije

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -1,13 +1,26 @@
 use crate::strukture::{Mark,Status,Tile,Mreza};
 
+
 impl Mreza {
-    pub fn uncover_tile(&mut self, mesto: (u16,u16)) -> () {
+    pub fn uncover_tile(&mut self, mesto: (u16,u16)) {
         let f = |tile: &mut Tile| {tile.uncover()};
         self.apply_on_tile(f , mesto)
     }
 
-    pub fn change_flag(&mut self, mesto: (u16,u16)) -> () {
+    pub fn change_flag(&mut self, mesto: (u16,u16)) {
         let f = |tile: &mut Tile| {tile.change_flag()};
         self.apply_on_tile(f , mesto)
     } 
+
+    pub fn empty_uncover(&mut self, mesto: (u16,u16)) {
+        // TODO
+    }
+
+    pub fn bomb_uncover(&mut self, mesto: (u16,u16)) {
+        // TODO
+    }
+
+    pub fn sure_uncover(&mut self, mesto: (u16,u16)) {
+        // TODO
+    }
 }
