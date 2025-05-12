fc06ca6acda8ab3440e0586106316b9fecaa2e30
Jan Verovnik <janverovnik4@gmail.com>
2025-05-12 18:46:01 +0200

    Sauron deluje brez rand dependencija!!!

--- src/generator_polja.rs
+++ src/generator_polja.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+// use rand::Rng;
 use crate::strukture::{Tile,Mreza};
 
 fn razlika(m:u16,n:u16) -> u16 {
@@ -76,22 +76,23 @@ impl Mreza {
 
 
 fn random_array(mut st_vseh:u16,mut st_min:u16) -> Vec<bool> {
-    let mut rng = rand::rng();
-    let mut nakljucno = vec![];
-    loop {
-        if rng.random_ratio(st_min.into(),st_vseh.into()) {
-            nakljucno.push(true);
-            st_min -= 1;
-            st_vseh -= 1;
-        } else {
-            nakljucno.push(false);
-            st_vseh -= 1
-        }
-        if st_vseh == 0 {
-            break;
-        }
-    }
-    nakljucno
+    // let mut rng = rand::rng();
+    // let mut nakljucno = vec![];
+    // loop {
+    //     if rng.random_ratio(st_min.into(),st_vseh.into()) {
+    //         nakljucno.push(true);
+    //         st_min -= 1;
+    //         st_vseh -= 1;
+    //     } else {
+    //         nakljucno.push(false);
+    //         st_vseh -= 1
+    //     }
+    //     if st_vseh == 0 {
+    //         break;
+    //     }
+    // }
+    // nakljucno
+    vec![]
 }
 
 #[cfg(test)]
