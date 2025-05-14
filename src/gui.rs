bdf7a50cde92ddcad3e6fb3707a0b246e37bae2d
MatejFMF <mk54674@student.uni-lj.si>
2025-05-14 08:34:53 +0200

    Dodal index.html

--- src/gui.rs
+++
@@ -1,3 +0,0 @@
-use sauron::{
-    html::text, html::units::px, jss, node, wasm_bindgen, Application, Cmd, Node, Program,
-};
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,29 @@
-mod display;
-mod generator_polja;
-mod strukture;
-mod gui;
+// mod display;
+// mod generator_polja;
+// mod strukture;
+// mod gui;
+
+use sauron::{node, wasm_bindgen, Application, Cmd, Node, Program};
+
+struct App;
+
+impl Application for App {
+    type MSG = ();
+
+    fn view(&self) -> Node<()> {
+        node! {
+            <p>
+                "hello"
+            </p>
+        }
+    }
+
+    fn update(&mut self, _msg: ()) -> Cmd<()> {
+        Cmd::none()
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    Program::mount_to_body(App);
+}
