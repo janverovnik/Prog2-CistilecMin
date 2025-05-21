de5e8b4fb7ced303a052e07d493640459539e73b
janverovnik <janverovnik4@gmail.com>
2025-05-21 09:40:25 +0200

    Updatan bolkjši uncover.

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -1,27 +1,30 @@
-use crate::strukture::{Mark,Status,Tile,Mreza, Vsebina};
+use sauron::StatefulComponent;
 
+use crate::strukture::{Mark,Status,Tile,Mreza, Vsebina};
 
 impl Mreza {
     pub fn uncover_tile(&mut self, mesto: (u16,u16)) {
         let f = |tile: &mut Tile| {tile.uncover()};
-        self.apply_on_tile(f , mesto)
+        match self.tile(mesto) {
+            None => (),
+            Some(tile) => match *tile.vsebina(){
+                Vsebina::Stevilo(x) => {self.apply_on_tile(f, mesto);
+                                            if x == 0 {for sosed in self.sosedje(mesto) {
+                                                self.apply_on_tile(f, sosed);
+                                            }}
+                },
+                Vsebina::Mina => for mina in self.mines(){
+                    self.apply_on_tile(f, mesto)
+                },
+
+            }
+        }
     }
 
     pub fn change_flag(&mut self, mesto: (u16,u16)) {
         let f = |tile: &mut Tile| {tile.change_flag()};
-        self.apply_on_tile(f , mesto)
+        self.apply_on_tile(f, mesto)
     } 
-
-    pub fn empty_uncover(&mut self, mesto: (u16,u16)) { // uncover za prazne tile-e, se odprejo vsi okoli itd.
-        let f = |tile: &mut Tile| {tile.uncover()};
-        for sosed in self.sosedje(mesto) {
-            self.apply_on_tile(f, sosed);
-        }
-    }
-
-    pub fn mine_uncover(&mut self, mesto: (u16,u16)) {
-        // TODO
-    }
 
     pub fn sure_uncover(&mut self, mesto: (u16,u16)) { // če imaš dva zaprta sosednja tile-a flaggana za mino lahko na tile dvakrat pritisneš da se odprejo ostali (at your own peril)W
         match self.tile(mesto) {
