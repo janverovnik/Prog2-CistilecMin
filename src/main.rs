3c29527d37d456e765befe3e526df44cf71b0267
MatejFMF <mk54674@student.uni-lj.si>
2025-05-21 15:54:00 +0200

    Dokončal prototip main funkcije

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,19 @@ mod display;
 mod gameplay;
 use std::io;
 
-// use crate generator_polja::{safe_new};
 use crate::strukture::{Tile,Mreza};
-use crate::gameplay;
+
 
 
 fn main() {
     println!("Select seed");
 
     let mut seed = String::new();
-    let mut poteza = String::new();
-
+    
     io::stdin()
-        .read_line(&mut seed)
-        .expect("Failed to read line");
+    .read_line(&mut seed)
+    .expect("Failed to read line");
+    
 
     let seed: u64 = match seed.trim().parse() {
         Ok(num) => num,
@@ -29,21 +28,34 @@ fn main() {
     loop {
         print!("{}", mreza);
         println!("Naredi potezo!");
+        
+        let mut poteza = String::new();
 
         io::stdin()
         .read_line(&mut poteza)
         .expect("Failed to read line");
 
-        let pot: Option<(char, u16, u16)> = match poteza.trim().parse() {
-        Ok(num) => Some(num),
-        Err(_) => None,
-    };
-    match pot {
-        | None => continue,
-        | Some(('U', x, y)) | Some (('u', x, y)) => mreza.change_flag((x,y)),
-        | Some(('F', x, y)) | Some (('f', x, y)) => mreza.uncover_tile((x,y)),
-        | _ => continue
-    }    
+        let mut iter = poteza.split_whitespace();
+        let crka_opt = iter.next();
+        let x_opt = iter.next();
+        let y_opt = iter.next();
+
+        let (crka,x,y) = match (crka_opt,x_opt,y_opt) {
+            (Some(crka),Some(x),Some(y)) => (crka.parse(),x.parse(),y.parse()),
+            _ => (Ok('X'),Ok(42),Ok(42)),
+        };
+
+        let pot : Option<(char,u16,u16)> = match (crka,x,y) {
+            (Ok(crka),Ok(x),Ok(y)) => Some((crka,x,y)),
+            _ => None,
+        };
+        
+        match pot {
+            | None => continue,
+            | Some(('U', x, y)) | Some (('u', x, y)) => mreza.uncover_tile((x,y)),
+            | Some(('F', x, y)) | Some (('f', x, y)) => mreza.change_flag((x,y)),
+            | _ => continue
+        }    
     }
 
 }
