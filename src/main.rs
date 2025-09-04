9d2118c03ad90779b181a1a2b392a5daec0bde2d
MatejFMF <mk54674@student.uni-lj.si>
2025-09-04 11:26:16 +0200

    Čiščenje kode

--- src/main.rs
+++ src/main.rs
@@ -73,31 +73,24 @@ mod splash {
 
     use super::{despawn_screen, GameState};
 
-    // This plugin will display a splash screen with Bevy logo for 1 second before switching to the menu
     pub fn splash_plugin(app: &mut App) {
-        // As this plugin is managing the splash screen, it will focus on the state `GameState::Splash`
+
         app
-            // When entering the state, spawn everything needed for this screen
             .add_systems(OnEnter(GameState::Splash), splash_setup)
-            // While in this state, run the `countdown` system
             .add_systems(Update, countdown.run_if(in_state(GameState::Splash)))
-            // When exiting the state, despawn everything that was spawned for this screen
             .add_systems(OnExit(GameState::Splash), despawn_screen::<OnSplashScreen>);
     }
 
-// Tag component used to tag entities added on the splash screen
 #[derive(Component)]
 struct OnSplashScreen;
 
-     // Newtype to use a `Timer` for this screen as a resource
 #[derive(Resource, Deref, DerefMut)]
 struct SplashTimer(Timer);
 
- // Newtype to use a `Timer` for this screen as a resource
 
 fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         let icon = asset_server.load("mina.png");
-        // Display the logo
+        // Display logo
         commands.spawn((
             Node {
                 align_items: AlignItems::Center,
@@ -110,7 +103,6 @@ fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             children![(     
                 ImageNode::new(icon),
                 Node {
-                    // This will set the logo to be 200px wide, and auto adjust its height
                     width: Val::Px(200.0),
                     ..default()
                 },
